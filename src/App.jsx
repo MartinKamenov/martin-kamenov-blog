c6bbecc889a5d9279130dee6a7828ae1b8a69249
MartinKamenov <martinkamenov10@gmail.com>
2019-12-01 12:53:47 +0200

    Add  graphql provider to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import RouterComponent from './components/routing/RouterComponent';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+const apolloClient = new ApolloClient({
+    uri: 'http://localhost:5000/graphql'
+});
 
 const App = () => (
-    <RouterComponent/>
+    <ApolloProvider client={apolloClient}>
+        <RouterComponent/>
+    </ApolloProvider>
 );
 
 export default App;
