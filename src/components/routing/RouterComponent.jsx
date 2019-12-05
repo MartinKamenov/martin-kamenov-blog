67848d8c94d049af62f17c170112a1ac06cbe7a8
MartinKamenov <martinkamenov10@gmail.com>
2019-12-05 19:34:20 +0200

    Add about me page

--- src/components/routing/RouterComponent.jsx
+++ src/components/routing/RouterComponent.jsx
@@ -3,14 +3,17 @@ import Home from '../home/Home';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from '../common/navbar/Navbar';
 
-import './RoutingComponent.css';
 import SinglePost from '../posts/single/SinglePost';
+import AboutMe from '../about-me/AboutMe';
+
+import './RoutingComponent.css';
 
 const RouterComponent = () => {
     return (
         <Router>
             <Navbar/>
             <Route exact path="/" component={Home} />
+            <Route exact path="/aboutme" component={AboutMe} />
             <Route path="/posts/:id" component={SinglePost} />
         </Router>
     );
