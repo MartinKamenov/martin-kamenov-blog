5b64391c5823cf40ac9f26d7e98c11a631cb8121
MartinKamenov <martinkamenov10@gmail.com>
2019-12-08 20:16:59 +0200

    Update links to social platforms

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -6,15 +6,15 @@ import './SocialButtons.css';
 
 const Footer = () => {
     return (
-        <nav className='social-btn-container'>
-            <div className="social-btns">
+        <nav className='social-btn-container slow-transition-container'>
+            <div className='social-btns'>
                 <h3>Find me on social media</h3>
-                <a className="btn facebook" href="https://www.facebook.com/martykam"><FaFacebook className="fa fa-facebook"/></a>
-                <a className="btn twitter" href="https://twitter.com"><FaTwitter className="fa fa-twitter"/></a>
-                <a className="btn google" href="mailto:martinkamenov10@gmail.com"><FaGoogle className="fa fa-google"/></a>
+                <a className='btn facebook' href='https://www.facebook.com/martykam'><FaFacebook className='fa fa-facebook'/></a>
+                <a className='btn twitter' href='https://twitter.com/MartinKamenov3'><FaTwitter className='fa fa-twitter'/></a>
+                <a className='btn google'  href='mailto:martinkamenov10@gmail.com?subject=Hello Martin&body=Hi.' target='_blank' rel='noopener noreferrer'><FaGoogle className='fa fa-google'/></a>
             </div>
         </nav>
     );
-}
+};
  
 export default Footer;
