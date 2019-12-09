c0e012da784b029719369d2212674ce9eddf10ab
MartinKamenov <martinkamenov10@gmail.com>
2019-12-09 19:05:13 +0200

    Open links in new tab

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
         <nav className='social-btn-container slow-transition-container'>
             <div className='social-btns'>
                 <h3>Find me on social media</h3>
-                <a className='btn facebook' href='https://www.facebook.com/martykam'><FaFacebook className='fa fa-facebook'/></a>
-                <a className='btn twitter' href='https://twitter.com/MartinKamenov3'><FaTwitter className='fa fa-twitter'/></a>
+                <a className='btn facebook' href='https://www.facebook.com/martykam' target='_blank'><FaFacebook className='fa fa-facebook'/></a>
+                <a className='btn twitter' href='https://twitter.com/MartinKamenov3' target='_blank'><FaTwitter className='fa fa-twitter'/></a>
                 <a className='btn google'  href='mailto:martinkamenov10@gmail.com?subject=Hello Martin&body=Hi.' target='_blank' rel='noopener noreferrer'><FaGoogle className='fa fa-google'/></a>
             </div>
         </nav>
