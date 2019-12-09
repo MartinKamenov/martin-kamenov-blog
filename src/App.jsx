67f106340fd9f5b03b51d31e239847d0be125653
MartinKamenov <martinkamenov10@gmail.com>
2019-12-09 13:31:52 +0200

    Add redux provider to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,38 @@ import React from 'react';
 import RouterComponent from './components/routing/RouterComponent';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import uuid from 'uuid';
+
+let token = localStorage.getItem('likes-token');
+if(!token) {
+    localStorage.setItem('likes-token', uuid.v1);
+}
+
+const reducer = (state = { token }, action) => {
+    const stateCopy = {...state};
+    switch(action.type) {
+        default:
+            return stateCopy;
+    }
+};
+
+const store = createStore(reducer);
 
 const apolloClient = new ApolloClient({
     uri: 'https://martin-kamenov-blog-api.herokuapp.com/graphql'
 });
 
 const App = () => (
-    <ApolloProvider client={apolloClient}>
-        <RouterComponent/>
-    </ApolloProvider>
+    <Provider store={store}>
+        <ApolloProvider client={apolloClient}>
+            <RouterComponent/>
+        </ApolloProvider>
+    </Provider>
 );
 
 export default App;
