618f1982eb7658474be5f9b64d6c3725c230514c
MartinKamenov <martinkamenov10@gmail.com>
2019-12-10 20:12:36 +0200

    Add new comment to state

--- src/components/posts/single/comments/CommentSection.jsx
+++ src/components/posts/single/comments/CommentSection.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import './CommentSection.css';
 
 const CommentSection = ({ comments }) => {
+    const [newComment, setNewComment] = useState({
+        username: '',
+        text: ''
+    });
+
+    const updateNewComment = (field, value) => {
+        const commentCopy = Object.assign({}, newComment);
+        commentCopy[field] = value;
+        setNewComment(commentCopy);
+    };
+
     return (
-        <div className='comment-section-container center-container'>
+        <div className='comment-section-container center-container container'>
             <h2>Comments</h2>
+            <div className='new-comment-container'>
+                <div style={{ width: '100%' }}>
+                    <TextField
+                        style={{ minWidth: '300px', width: '70%' }}
+                        label="Name"
+                        variant="outlined"
+                        value={newComment.username}
+                        onChange={({ target: { value } }) => updateNewComment('username', value)} />
+                </div>
+                <div style={{ marginTop: 10, width: '100%' }}>
+                    <TextField
+                        style={{ minWidth: '300px', width: '70%' }}
+                        multiline
+                        rows={3}
+                        label="Type some comment"
+                        variant="outlined"
+                        value={newComment.text}
+                        onChange={({ target: { value } }) => updateNewComment('text', value)} />
+                </div>
+            </div>
             {
                 comments.map((comment, i) => (
                     <div key={i}>
